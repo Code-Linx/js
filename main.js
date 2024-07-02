2cd830ab5d75a4153dcacb10ed28714992ed7528
Dennis <104856811+Code-Linx@users.noreply.github.com>
2024-07-02 21:28:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1132,3 +1132,17 @@ let fasterShip = {
 'Fuel Type' : 'Turbo Fuel',
 color : 'silver'
 }
+
+
+
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+// Write your code below
+let crewCount = spaceship.numCrew;
+let planetArray = spaceship.flightPath;
