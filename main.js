679bd270fba0e1129aaf33e135eaebe53866263f
Dennis <104856811+Code-Linx@users.noreply.github.com>
2024-07-03 17:53:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1146,3 +1146,17 @@ let spaceship = {
 // Write your code below
 let crewCount = spaceship.numCrew;
 let planetArray = spaceship.flightPath;
+
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth', 
+  numCrew: 5
+ };
+
+let propName =  'Active Mission';
+
+// Write your code below
+let isActive = spaceship['Active Mission'];
+console.log(isActive)
