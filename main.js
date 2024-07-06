5532685d8767ad64bee4e54781a572359b93f819
Dennis <104856811+Code-Linx@users.noreply.github.com>
2024-07-06 12:38:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1160,3 +1160,16 @@ let propName =  'Active Mission';
 // Write your code below
 let isActive = spaceship['Active Mission'];
 console.log(isActive)
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.',
+ 
+};
+
+// Write your code below
+spaceship.color = 'glorious gold';
+spaceship.numEngines = 1;
+delete spaceship['Secret Mission'];
