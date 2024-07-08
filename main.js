2a889aedfbc19a6121abed2b918f520b0c9d6c95
Dennis <104856811+Code-Linx@users.noreply.github.com>
2024-07-08 20:19:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1173,3 +1173,19 @@ let spaceship = {
 spaceship.color = 'glorious gold';
 spaceship.numEngines = 1;
 delete spaceship['Secret Mission'];
+
+
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+let alienShip = {
+  retreat () {
+    console.log(retreatMessage)
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+};
+
+alienShip.retreat();
+alienShip.takeOff()
