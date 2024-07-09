5c48768e7e0c91823f814f63895c3dd1dcecd0d0
Dennis <104856811+Code-Linx@users.noreply.github.com>
2024-07-09 18:57:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1189,3 +1189,32 @@ let alienShip = {
 
 alienShip.retreat();
 alienShip.takeOff()
+
+
+const spaceship = {
+     telescope: {
+        yearBuilt: 2018,
+        model: '91031-XLT',
+        focalLength: 2032 
+     },
+    crew: {
+        captain: { 
+            name: 'Sandra', 
+            degree: 'Computer Engineering', 
+            encourageTeam() { console.log('We got this!') } 
+         }
+    },
+    engine: {
+        model: 'Nimbus2000'
+     },
+     nanoelectronics: {
+         computer: {
+            terabytes: 100,
+            monitors: 'HD'
+         },
+        'back-up': {
+           battery: 'Lithium',
+           terabytes: 50
+         }
+    }
+}; 
