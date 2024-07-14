2f582bd59e553f40ecd8835acb087a65859d2c91
Dennis <104856811+Code-Linx@users.noreply.github.com>
2024-07-14 23:57:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1274,4 +1274,26 @@ let paintIt = obj => {
 paintIt(spaceship);
  
 spaceship.color // Returns 'glorious gold'
+
+
+let spaceship = {
+  homePlanet : 'Earth',
+  color : 'red'
+};
+let tryReassignment = obj => {
+  obj = {
+    identified : false, 
+    'transport type' : 'flying'
+  }
+  console.log(obj) // Prints {'identified': false, 'transport type': 'flying'}
+ 
+};
+tryReassignment(spaceship) // The attempt at reassignment does not work.
+spaceship // Still returns {homePlanet : 'Earth', color : 'red'};
+ 
+spaceship = {
+  identified : false, 
+  'transport type': 'flying'
+}; // Regular reassignment still works.
+
  
