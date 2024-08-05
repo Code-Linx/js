af2fd9f722aea37e512f22a3c7b06c5547a14795
Dennis <104856811+Code-Linx@users.noreply.github.com>
2024-08-05 18:49:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1377,6 +1377,9 @@ for (let crewMember in spaceship.crew) {
   console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
 }
 
+for (let crewMember in spaceship.crew) {
+  console.log(`${spaceship.crew[crewMember].name} : ${spaceship.crew[crewMember].degree}`)
+}
 // Write your code below
 
 
