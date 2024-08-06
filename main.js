fc015e97ca01b0a99f5bb64bec7d55cc81411be8
Dennis <104856811+Code-Linx@users.noreply.github.com>
2024-08-06 19:53:56 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1383,4 +1383,23 @@ for (let crewMember in spaceship.crew) {
 // Write your code below
 
 
+let halley = {
+  _name: 'Halley',
+  _behavior: 0,
+  
+  get name() {
+    return this._name;
+  },
+  
+  get behavior() {
+    return this._behavior;
+  },
+    
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
+
+
+
 
