718e03a2bc1c21ee81cf69f66973d41d5f58b927
Dennis <104856811+Code-Linx@users.noreply.github.com>
2024-08-23 21:00:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1427,3 +1427,12 @@ console.log(halley.behavior); // Print behavior value to console
 halley.incrementBehavior(); // Add one to behavior
 console.log(halley.name); // Print name value to console
 onsole.log(halley.behavior); // Print behavior value to console
+
+
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
